de17a5bac3f26f3c5d252c048fde3dadee748759
MK-NG <michaeljfknight@gmail.com>
2021-08-16 14:06:04 +0100

    first commit

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Post-16 options',
 
   // Default port that prototype runs on
   port: '3000',
